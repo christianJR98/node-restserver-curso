16feb3b37e1c1ec748dce70d57e9c6fac90c464f
christianJR98 <cristian-1823@live.com.mx>
2020-05-09 13:51:57 -0500

    primer commit

---
+++ server/config/config.js
@@ -0,0 +1,8 @@
+//Es un objeto global en node que corre a lo largo de todo node
+//y se actualiza decuerdo al enviroment
+
+// =================================
+//  PUERTO
+// =================================
+
+process.env.PORT = process.env.PORT || 3000;
